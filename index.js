8d4278331852a9e15f4b91afcd2e9b9def066cb0
Avinash Devineni <devinenia69420@gmail.com>
2024-01-24 21:43:33 -0600

    Initial commit; just need to style

---
+++ index.js
@@ -0,0 +1,39 @@
+//#region INPUT
+const latitudeInput = document.getElementById("latitude");
+const longitudeInput = document.getElementById("longitude");
+
+const isCelsiusRadio = document.getElementById("celsius");
+isCelsiusRadio.checked = true;
+const isFahrenheitRadio = document.getElementById("fahrenheit");
+//#endregion
+
+const getWeatherButton = document.getElementById("get-weather");
+
+//#region OUTPUT
+const temperatureOutput = document.getElementById("weather-output");
+//#endregion
+
+getWeatherButton.addEventListener('click', _event => {
+    temperatureOutput.textContent = "Loading...";
+
+    fetch(`https://api.open-meteo.com/v1/gfs?latitude=${latitudeInput.value}&longitude=${longitudeInput.value}&hourly=temperature_2m&temperature_unit=${isCelsiusRadio.checked ? "celsius" : "fahrenheit"}&wind_speed_unit=ms&precipitation_unit=inch&forecast_days=1`)
+    .then(_response => _response.json()).
+      then(_response => {
+        let _currentHour = new Date().getHours();
+        temperatureOutput.textContent = `Temperature: ${_response.hourly.temperature_2m[_currentHour - 1]}°
+        ${isCelsiusRadio.checked ? "C" : "F"}`;
+
+        let _tempInFahrenheit = _response.hourly.temperature_2m[_currentHour - 1];
+        if (isCelsiusRadio.checked) {
+            _tempInFahrenheit = _tempInFahrenheit * 9 / 5 + 32;
+        }
+
+        if (_tempInFahrenheit <= 25) {
+            temperatureOutput.textContent += " ❄";
+        }
+
+        else if (_tempInFahrenheit >= 81.5) {
+            temperatureOutput.textContent += " 🔥";
+        }
+    }).catch(error => { console.error(error); temperatureOutput.textContent = "An error occured. Please try again." });
+});
